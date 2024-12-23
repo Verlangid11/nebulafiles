0895c023465568f2bc9404b391590f0e6d2de1e5
verlangid <165430045+Verlangid11@users.noreply.github.com>
2024-12-23 15:09:55 +0700

    Update nebula.sh

--- nebula.sh
+++ nebula.sh
@@ -1,45 +1,39 @@
 cd /var/www/pterodactyl
-php artisan down
-curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+ php artisan down
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv 
 
-chmod -R 755 storage/* bootstrap/cache
+chmod -R 755 storage/* bootstrap/cache 
 
 composer install --no-dev --optimize-autoloader --no-interaction
 
 php artisan view:clear
+
 php artisan config:clear
+
 php artisan migrate --seed --force
 chown -R www-data:www-data /var/www/pterodactyl/*
 php artisan up
-echo "Semua tema dan addon telah dihapus."
-echo "PROSES PEMASANGAN NEBULA THEME"
-
-# Pastikan Node.js kompatibel (gunakan Node.js 18)
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+        echo "Semua tema dan addon telah dihapus."
+        echo "PROSES PEMASANGAN NEBULA THEME"
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+sudo rm -f /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install -y nodejs
 npm i -g yarn
-
 cd /var/www/pterodactyl
-yarn install --force
+yarn
 yarn add cross-env
-
 apt install -y zip unzip git curl wget
 wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
-unzip -o release.zip -d /var/www/pterodactyl
-
-# Jalankan Blueprint
-chmod +x blueprint.sh
-bash blueprint.sh < <(yes "y")
-
-# Pastikan direktori ada
-sudo mkdir -p /var/www/pterodactyl/.blueprint/extensions/nebula/public
-
-# Perbaiki kepemilikan direktori
-sudo chown -R www-data:www-data /var/www/pterodactyl
-
-# Ubah hak akses
-sudo chmod -R 755 /var/www/pterodactyl/.blueprint/extensions/nebula/public
-
-# Instalasi Nebula Theme langsung pake dev/null
+mv release.zip var/www/pterodactyl/release.zip
 cd /var/www/pterodactyl
-blueprint -install nebula < /dev/null
+yes A | unzip release.zip
+WEBUSER="www-data"; USERSHELL="/bin/bash"; PERMISSIONS="www-data:www-data";
+sed -i -E -e "s|WEBUSER=\"www-data\" #;|WEBUSER=\"$WEBUSER\" #;|g" -e "s|USERSHELL=\"/bin/bash\" #;|USERSHELL=\"$USERSHELL\" #;|g" -e "s|OWNERSHIP=\"www-data:www-data\" #;|OWNERSHIP=\"$PERMISSIONS\" #;|g" $FOLDER/blueprint.sh
+chmod +x blueprint.sh
+bash /var/www/pterodactyl/blueprint.sh < <(yes "y")
+cd /var/www/
+cd /var/www/pterodactyl && blueprint -install nebula < /dev/null
